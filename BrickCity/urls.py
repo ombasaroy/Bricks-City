154d6de52228b8b7d2e846ef48c42711ef935af5
ombasaroy <37172390+ombasaroy@users.noreply.github.com>
2024-04-08 23:03:58 +0300

    Fixed about, contact, refined index

--- BrickCity/urls.py
+++ BrickCity/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('services/', views.services, name='services'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+
 ]
--- BrickCity/views.py
+++ BrickCity/views.py
@@ -4,4 +4,20 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'base.html')
+    context = {"nav": 'index'}
+    return render(request, 'index.html', context)
+
+
+def services(request):
+    context = {"nav": 'services'}
+    return render(request, 'services.html', context)
+
+
+def about(request):
+    context = {"nav": 'about'}
+    return render(request, 'about.html', context)
+
+
+def contact(request):
+    context = {"nav": 'contact'}
+    return render(request, 'contact.html', context)
