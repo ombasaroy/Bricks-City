29c2a3a98ef850c32a255d5009091cdf0707c727
ombasaroy <37172390+ombasaroy@users.noreply.github.com>
2024-04-11 10:25:06 +0300

    Started the admin panel

--- BrickCity/urls.py
+++ BrickCity/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     path('partnerships/', views.partnerships, name='partnerships'),
     path('blog/', views.blog, name='blog'),
     path('single-blog/', views.single_blog, name='single_blog'),
+    path('bricksadmin/', views.bricksadmin, name='bricksadmin'),
+    path('adminlogin/', views.adminlogin, name='adminlogin'),
+    path('adminsignup/', views.adminsignup, name='adminsignup'),
 
 ]
--- BrickCity/views.py
+++ BrickCity/views.py
@@ -5,32 +5,49 @@ from django.shortcuts import render
 
 def index(request):
     context = {"nav": 'index'}
-    return render(request, 'index.html', context)
+    return render(request, 'bricks/index.html', context)
 
 
 def services(request):
     context = {"nav": 'services'}
-    return render(request, 'services.html', context)
+    return render(request, 'bricks/services.html', context)
 
 
 def about(request):
     context = {"nav": 'about'}
-    return render(request, 'about.html', context)
+    return render(request, 'bricks/about.html', context)
 
 
 def contact(request):
     context = {"nav": 'contact'}
-    return render(request, 'contact.html', context)
+    return render(request, 'bricks/contact.html', context)
 
 
 def partnerships(request):
-    return render(request, 'partnerships.html')
+    return render(request, 'bricks/partnerships.html')
 
 
 def blog(request):
     context = {'nav': 'blog'}
-    return render(request, 'blog.html', context)
+    return render(request, 'bricks/blog.html', context)
 
 
 def single_blog(request):
-    return render(request, 'single-blog.html')
+    return render(request, 'bricks/single-blog.html')
+
+
+# ADMIN STARTS HERE
+def bricksadmin(request):
+    return render(request, 'bricksadmin/home.html')
+
+
+def adminlogin(request):
+    return render(request, 'bricksadmin/login.html')
+
+
+def adminsignup(request):
+    return render(request, 'bricksadmin/signup.html')
+
+
+# ADMIN ENDS HERE
+
